f689da3f6d27ccf7c2ea09f2c9e2429baa141c6a
rezaigh <mohammadreza.ilchi@gmail.com>
2026-06-29 13:00:25 +0330

    Update MainActivity.kt

--- app/src/main/java/com/smoothvpn/ui/MainActivity.kt
+++ app/src/main/java/com/smoothvpn/ui/MainActivity.kt
@@ -29,6 +29,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.foundation.Canvas
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
